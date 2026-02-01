3ace54611e86e1fe1043fe706656cd795df34952
piyapat <piyapat.thongrueng@gmail.com>
2026-02-02 01:26:20 +0700

    chore: add package.json and vercel.json for server setup

---
+++ app.mjs
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+app.use(cors());
+app.use(express.json());
+
+
+app.get("/profiles", (req, res) => {
+  return res.status(200).json({
+    data: {
+      name: "john",
+      age: 20,
+    },
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
